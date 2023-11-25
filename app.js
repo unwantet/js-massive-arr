6ee217a24ac0a4c83a03afe9d8a24d4999b27735
unwantet <darkblezer59@gmail.com>
2023-11-25 20:23:17 +0500

    add hw

---
+++ app.js
@@ -0,0 +1,292 @@
+
+////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// function boshStr(str) {
+//     if (str == "") {
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);
+//     }
+    
+// }
+// boshStr("")
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+
+
+// function bolinsin5(son) {
+//     if (son % 5 == 0) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+        
+//     }
+
+// }
+// bolinsin5(52)
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+// function bolinsin100(son) {
+//     if (son % 100 == 0) {
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);
+
+//     }
+
+// }
+// bolinsin100(200)
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+// function uzunlik(str) {
+//     let b = 0;
+//     for(let a = 0 ; a < str.length ; a++){
+//         b++;
+//     }
+//     console.log(b);
+
+// }
+
+// uzunlik("hello world")
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+// function bolinsin(x, y) {
+//     if (x > y) {
+//         if (x % y == 0) {
+//             console.log(true);
+//         } 
+//         else{
+//             console.log(false);
+//         }
+        
+//     }
+//     else{
+//         console.log(false);
+//     }
+
+// }
+// bolinsin(4,85)
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+// function raqam(str) {
+//     console.log(str.toString());    
+    
+// }
+// raqam("1221")
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+// function tortYuzi(a, b) {
+//     if (a > 0 & b > 0) {
+//         return console.log(a * b);
+//     }
+//     else{
+//         console.log(-1);
+//     }
+// }
+// tortYuzi(5,0)
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+// function ismFam(ism, familiya) {
+//     console.log(` ${familiya} , ${ism} `);    
+// }
+
+// ismFam("aliwer","solijonov");
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// function bol(inp) {
+//     if (inp == true) {
+//         console.log("sad days");
+//     } else {
+//         console.log("it's a good day");
+//     }
+// }
+// bol(false)
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+// function negativ(son) {
+//     if (son > 0) {
+//         console.log(son - (son*2));
+//     }else if (son == 0){
+//         console.log(son);
+//     }else{
+//         console.log(son + (son*(-1)*2));
+//     }
+
+// }
+
+// negativ(25)
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
+// let holatiYaxshimi = true
+// let holati;
+
+// let hy = (holatiYaxshimi) ? holati = 'yaxshi' : holati = 'yaxshi emas';
+
+// console.log(holati);
+
+////////////////////////////////////////////
+
+// function sozUzunligi(str) {
+//     let b = 0;
+//     for(let a = 0; a < str.length ; a++){
+//         b++;
+//     }    
+//     if (b % 2 == 0) {
+//         console.log(true);
+//     }else{
+//         console.log(false);
+
+//     }
+// }
+
+// sozUzunligi("salomts")
+
+//////////////////////////////////////////
+
+
+// function daraja(x, y) {
+//     console.log(x ** y);
+// }
+// daraja(3,4)
+
+////////////////////////////////////////
+
+
+// function songgiElement(massiv) {
+    
+//     if (massiv.length === 0) {
+//         console.log(undefined);
+//     } else {
+//         console.log( massiv[massiv.length - 1]);
+//     }
+// }
+// songgiElement("salom dunyo")
+
+
+//////////////////////////////////////////////
+
+
+// function kabisa(yil) {
+
+//     if (yil % 100==0  &&  yil % 400== 0) {
+//         return console.log(true);
+//     }
+//     else if (yil % 4 == 0) {
+//         return console.log(true);
+//     }else{
+//         return console.log(false);
+
+//     }
+//     }
+// kabisa(700)
+
+
+//////////////////////////////////////////////////
+
+
+// function birinchiHarfIsiz(soz) {
+//     if (typeof soz === 'string' && soz.length > 0) {
+//         return console.log(soz.substring(1));
+//     } else {
+//         return console.log(soz); 
+// }
+
+// birinchiHarfIsiz('salomat')
+
+////////////////////////////////////////////////////////////
+
+
+
+// function bool(inp) {
+//     if (inp == true) {
+//         console.log(false);
+//     } else{
+//         console.log(true);
+//     }
+// }
+// bool(fasle)
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// function elementniOlish(massiv, index) {
+//     if (Array.isArray(massiv) && index >= 0 && index < massiv.length) {
+//         return massiv[index]; 
+//     } else {
+//         return undefined; 
+//     }
+// }
+
+// let testMassiv = [10, 20, 30, 40, 50];
+// let testIndex1 = 2;
+// let testIndex2 = 8;
+
+// let natija1 = elementniOlish(testMassiv, testIndex1);
+// let natija2 = elementniOlish(testMassiv, testIndex2);
+
+// console.log( testIndex1, "indexdagi elementi:", natija1); // Natija 30 bo'lishi kutilmoqda
+// console.log( testIndex2, "indexdagi elementi:", natija2); // Natija undefined bo'lishi kutilmoqda
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+// function juftMiToqmi(son) {
+//     if (son % 2 == 0){
+//         console.log("juft");
+//     } else{
+//         console.log("toq");
+
+//     }
+
+// }
+// juftMiToqmi(55)
+
+
+///////////////////////////////////////////////////////////////////////
+
+
+// function qutilar(qavat) {
+//     let a = qavat * qavat;
+//     console.log(`${qavat} qavatda karobkala ${a} ta` );
+// }
+// qutilar(6);
+
+
+///////////////////////////////////////////////////
+
